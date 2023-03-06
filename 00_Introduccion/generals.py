19a84ca7ea7e0eddbd7ed64cd46d4a36751d5374
Alejandro Morgan <unciafidelis@gmail.com>
2023-03-06 09:51:57 -0600

    Created Introduction folder with generals.py

---
+++ 00_Introduccion/generals.py
@@ -0,0 +1,20 @@
+# Introducción
+
+print(1 + 2)   # Suma(+)
+print(3 - 2)   # Resta(-)
+print(4 * 4)   # Multiplicación(*)
+print(5 / 3)   # División(/)
+print(6 ** 2)  # Exponencial(**)
+print(7 % 2)   # Módulo(%)
+print(7 // 2)  # División entera(//)
+
+# Tipos de datos
+
+print(type(1))                  # Int // Entero
+print(type(3.14159))                # Float // Flotante
+print(type(1j + 1 + 1j + 1))              # Complex // Complejo
+print(type('Python'))          # String // Cadena
+print(type([1, 2, 3]))           # List // Lista
+print(type({'curso':'Python'})) # Dictionary // Diccionario
+print(type({9.8, 3.14, 2.7}))    # Set // Conjunto
+print(type((9.8, 3.14, 2.7)))    # Tuple // Tupla
